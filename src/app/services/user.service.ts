86d7cb283caa0d817506c3f0cff5491f83937d52
Alicia <aliciaderradji@live.fr>
2021-08-08 20:13:32 +0200

    added profil and dashboard component + get all users

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  baseUrl = `${environment.baseUrl}/users`;
+
+    findAll(): Observable<Array<User>>{
+    return this.httpClient.get<Array<User>>( this.baseUrl);
+  }
+}
