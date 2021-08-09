492e15e0c3bcc235a1fbbd3da2840fe7889df494
Alicia <aliciaderradji@live.fr>
2021-08-09 21:02:58 +0200

    delete user

--- src/main/java/com/programming/techie/springngblog/controller/UserController.java
+++ src/main/java/com/programming/techie/springngblog/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.programming.techie.springngblog.controller;
 
 
+import com.programming.techie.springngblog.dto.PostDto;
+import com.programming.techie.springngblog.dto.UserDto;
+import com.programming.techie.springngblog.model.Post;
 import com.programming.techie.springngblog.model.User;
 import com.programming.techie.springngblog.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +24,31 @@ public class UserController {
     @Autowired
     UserDetailsServiceImpl userDetailsServiceImpl;
 
-
     @GetMapping("")
     public Iterable<User> listUser() {
-        return userDetailsServiceImpl.getAllUser();
+        return userDetailsServiceImpl.getAllUsers();
     }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userDetailsServiceImpl.deleteSinglePost(id);
+    }
+/*
+
+    @GetMapping("/{id}")
+    public User user(@PathVariable long id) { return  userDetailsServiceImpl.getUserById(id);}
+*/
+
+  /*  @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody UserDto userDto ) {
+        User user = userDetailsServiceImpl.updateSingleUser(userDto);
+        return new ResponseEntity(user, HttpStatus.OK);
+    }*/
+
+/*
+    @GetMapping("/{id}")
+    public User user(@PathVariable long id) { return  userDetailsServiceImpl.getUserById(id);}*/
+
 /*
 
     public UserController(UserService userService) {
--- src/main/java/com/programming/techie/springngblog/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/programming/techie/springngblog/service/impl/UserDetailsServiceImpl.java
@@ -1,8 +1,14 @@
 package com.programming.techie.springngblog.service.impl;
 
 
+import com.programming.techie.springngblog.dto.PostDto;
+import com.programming.techie.springngblog.dto.UserDto;
+import com.programming.techie.springngblog.exception.PostNotFoundException;
+import com.programming.techie.springngblog.model.Post;
 import com.programming.techie.springngblog.model.User;
 import com.programming.techie.springngblog.repository.UserRepository;
+import com.programming.techie.springngblog.service.AuthService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -11,21 +17,80 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.Instant;
 import java.util.Collection;
 import java.util.Collections;
 
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
-
 
     @Autowired
     private UserRepository userRepository;
+    private ModelMapper modelMapper = new ModelMapper();
 
-
-    public Iterable<User> getAllUser(){
+    public Iterable<User> getAllUsers(){
         return userRepository.findAll();
     }
+
+
+    public void deleteSinglePost(Long id){
+        User user = userRepository.findById(id).orElseThrow(() -> new PostNotFoundException("For id " + id));
+        userRepository.delete(user);
+    }
+
+  /*  public User getUserById(long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+*/
+
+  /*  public User updateSingleUser(UserDto userDto){
+        User user = modelMapper.map(userDto, User.class);
+        return userRepository.save(user);
+    }*/
+
+  /*  public UserDto getUserById(long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new PostNotFoundException("For id " + id));
+        return mapFromUserToDto(user);
+    }*/
+
+/*    public User getUserById(long id) {
+        return userRepository.findById(id).orElseThrow();
+    }*/
+
+
+/*    @Override
+    public PostDto readSinglePost(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException("For id " + id));
+        return mapFromPostToDto(post);
+    }*/
+
+/*
+    private PostDto mapFromUserToDto(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setEmail(user.getEmail());
+        userDto.setUsername(user.getUsername());
+
+        return userDto;
+    }
+*/
+
+/*    private Post mapFromDtoToPost(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setContent(postDto.getContent());
+        org.springframework.security.core.userdetails.User loggedInUser = authService.getCurrentUser().orElseThrow(() -> new IllegalArgumentException("User Not Found"));
+        post.setCreatedOn(Instant.now());
+        post.setUsername(loggedInUser.getUsername());
+        post.setUpdatedOn(Instant.now());
+        return post;
+    }*/
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
